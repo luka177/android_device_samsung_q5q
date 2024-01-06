bcf86c0860f564b5226909305af8f6d5f1080274
J0SH1X <aljoshua.hell@gmail.com>
2024-01-06 09:58:56 +0100

    Lineage -> DerpFest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023 The derpOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dm1q.mk
+    $(LOCAL_DIR)/derp_dm1q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dm1q-eng \
-    lineage_dm1q-user \
-    lineage_dm1q-userdebug
+    derp_dm1q-eng \
+    derp_dm1q-user \
+    derp_dm1q-userdebug
