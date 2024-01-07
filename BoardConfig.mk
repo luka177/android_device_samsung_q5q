118e160559faaa65de01561f00172a70c916b113
J0SH1X <aljoshua.hell@gmail.com>
2024-01-07 21:00:02 +0100

    build extmods

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,3 +14,39 @@ TARGET_OTA_ASSERT_DEVICE := dm1q
 
 # Display
 TARGET_SCREEN_DENSITY := 450
+
+# Kernel Modules
+#BOARD_SYSTEM_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/modules.load.system_dlkm))
+#BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.system_dlkm))
+#BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+#BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
+#BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_boot))
+#BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist.vendor_boot
+#BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+#BOOT_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery $(DEVICE_PATH)/modules.load.vendor_ramdisk))
+
+TARGET_KERNEL_EXT_MODULES := \
+  qcom/opensource/mmrm-driver \
+  qcom/opensource/mm-drivers/hw_fence \
+  qcom/opensource/mm-drivers/msm_ext_display \
+  qcom/opensource/mm-drivers/sync_fence \
+  qcom/opensource/audio-kernel \
+  qcom/opensource/camera-kernel \
+  qcom/opensource/dataipa/drivers/platform/msm \
+  qcom/opensource/datarmnet/core \
+  qcom/opensource/datarmnet-ext/aps \
+  qcom/opensource/datarmnet-ext/offload \
+  qcom/opensource/datarmnet-ext/shs \
+  qcom/opensource/datarmnet-ext/perf \
+  qcom/opensource/datarmnet-ext/perf_tether \
+  qcom/opensource/datarmnet-ext/sch \
+  qcom/opensource/datarmnet-ext/shs \
+  qcom/opensource/datarmnet-ext/wlan \
+  qcom/opensource/securemsm-kernel \
+  qcom/opensource/display-drivers/msm \
+  qcom/opensource/eva-kernel \
+  qcom/opensource/video-driver \
+  qcom/opensource/graphics-kernel \
+  qcom/opensource/wlan/platform \
+  qcom/opensource/wlan/qcacld-3.0/.kiwi_v2 \
+  qcom/opensource/bt-kernel
