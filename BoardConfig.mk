d2b513abdc472647d729b27876f31e31fed00b26
Luka Panio <lukapanio@gmail.com>
2024-02-29 11:44:30 +0100

    q5q: Switch to qca6490
    Change-Id: I7f1f972c6c14607e16f6f89f541c76fc021128c7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ TARGET_KERNEL_EXT_MODULES := \
   qcom/opensource/video-driver \
   qcom/opensource/graphics-kernel \
   qcom/opensource/wlan/platform \
-  qcom/opensource/wlan/qcacld-3.0/.kiwi_v2 \
+  qcom/opensource/wlan/qcacld-3.0/.qca6490 \
   qcom/opensource/bt-kernel
 
 # Recovery
